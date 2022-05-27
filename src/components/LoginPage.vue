ae44b378649d96c2d8f77e2cf56fd6e1b856acd4
AntunBelic <antunbelic93@gmail.com>
2022-05-27 09:08:47 +0200

    auth api call

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,12 +1,4 @@
 <template>
-    <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-50">
-    <body class="h-full">
-    ```
-  -->
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
             <div>
@@ -16,17 +8,19 @@
                     Sign in to your account
                 </h2>
             </div>
-            <form class="mt-8 space-y-6" action="#" method="POST">
+            <form @submit.prevent="handleLogin" class="mt-8 space-y-6" action="#" method="POST">
                 <div class="rounded-md shadow-sm -space-y-px">
                     <div>
                         <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required=""
+                        <input v-model="email" id="email-address" name="email" type="email" autocomplete="email"
+                            required=""
                             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Email address" />
                     </div>
                     <div>
                         <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                        <input v-model="password" id="password" name="password" type="password"
+                            autocomplete="current-password" required=""
                             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Password" />
                     </div>
@@ -42,5 +36,26 @@
     </div>
 </template>
 
-<script setup>
+<script>
+import axsios from "axios"
+
+export default {
+    name: "LoginPage",
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        async handleLogin() {
+            const response = await axsios.post("https://api.getcountapp.com/api/v1/authenticate", {
+                username: this.email,
+                password: this.password
+            })
+
+            console.log(response)
+        }
+    }
+}
 </script>
